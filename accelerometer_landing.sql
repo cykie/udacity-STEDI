420044aa49d6dc3c723c7b7a6585231fb2e0d5cd
cykie <fangyuan.0113@gmail.com>
2023-02-18 16:02:40 -0500

    Add files via upload

---
+++ accelerometer_landing.sql
@@ -0,0 +1,11 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi-project`.`accelerometer_landing` (
+  `user` string,
+  `timeStamp` bigint,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://udacity-stedi-yf/landing/'
+TBLPROPERTIES ('classification' = 'json');
